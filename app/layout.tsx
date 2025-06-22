df3fad0f0a5d3ab1d842ec04351abbd36dab0276
Dharam Soni <dharamsoni1010@gmail.com>
2025-06-22 16:45:15 +0530

    Initial app commit

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
+import { Inter, Playfair_Display } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const playfairDisplay = Playfair_Display({
+  variable: "--font-playfair-display",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Stratosphere | Cloud Storage Platform",
+  description:
+    "Stratosphere is a cloud storage platform that provides scalable and secure storage solutions for your data.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${inter.variable} ${playfairDisplay.variable} antialiased`}
+      >
+        <ThemeProvider attribute="class">{children}</ThemeProvider>
+      </body>
+    </html>
+  );
+}
