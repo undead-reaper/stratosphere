d200ba420fd02de4bae7d41cdb551e6dec2b97ed
Dharam Soni <dharamsoni1010@gmail.com>
2025-06-28 21:06:47 +0530

    Started dashboard layout

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,92 @@
-export default function Home() {
-  return <main></main>;
-}
+import { getFiles } from "@/appwrite/actions/file.actions";
+import { getCurrentUser } from "@/appwrite/actions/user.actions";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { getFormattedSize } from "@/lib/utils";
+import { AppwriteFileOutput } from "@/types/AppwriteFile";
+import { format, formatDistanceToNowStrict } from "date-fns";
+import { Models } from "node-appwrite";
+
+const Home = async () => {
+  const files: Models.DocumentList<AppwriteFileOutput> = await getFiles({
+    types: [],
+  });
+
+  const size = files?.documents.reduce((acc, file) => acc + file.size, 0) || 0;
+  const user = await getCurrentUser();
+
+  const totalSize = getFormattedSize({ size });
+  const now: Date = new Date();
+  const date = format(now, "EEEE, MMMM d, yyyy");
+  const recentFiles = files.documents.slice(0, 5);
+
+  return (
+    <div className="p-5 flex flex-col w-full h-full">
+      <Card className="mb-5">
+        <CardHeader>
+          <CardTitle>👋 Hello, {user?.fullName}!</CardTitle>
+          <CardDescription>{date}</CardDescription>
+        </CardHeader>
+      </Card>
+      <p className="text-muted-foreground">Total Storage</p>
+      <h1 className="text-xl font-bold">
+        {totalSize} <span className="text-base font-normal">used from 2GB</span>
+      </h1>
+      <h1 className="mb-5 font-playfair-display font-bold mt-5">
+        Recently Modified Files
+      </h1>
+      <Table>
+        <TableCaption>Your Recently Modified Files</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-min">File Name</TableHead>
+            <TableHead>Date Uploaded</TableHead>
+            <TableHead>Modified</TableHead>
+            <TableHead>File Size</TableHead>
+            <TableHead>File Owner</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {recentFiles.map((file) => (
+            <TableRow key={file.$id}>
+              <TableCell>{file.name}</TableCell>
+              <TableCell>{format(file.$createdAt, "dd/MM/yyyy")}</TableCell>
+              <TableCell>
+                {formatDistanceToNowStrict(file.$updatedAt)} ago
+              </TableCell>
+              <TableCell>{getFormattedSize({ size: file.size })}</TableCell>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <TableCell>{file.owner.fullName}</TableCell>
+                </TooltipTrigger>
+                <TooltipContent side="left">
+                  <p>{file.owner.email}</p>
+                </TooltipContent>
+              </Tooltip>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default Home;
